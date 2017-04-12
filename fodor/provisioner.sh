0d30bd2e61456c8dcb7c720f349b1b4b6c4b76b7
Ashley Hindle <hey@ashleyhindle.com>
2017-04-12 20:19:08 +0100

    Apt-get install with -y

--- fodor/provisioner.sh
+++ fodor/provisioner.sh
@@ -3,7 +3,7 @@ debconf-set-selections <<< "postfix postfix/mailname string ${DOMAIN}"
 debconf-set-selections <<< "postfix postfix/main_mailer_type string 'Internet Site'"
 
 apt-get update
-apt-get install ca-certificates curl postfix letsencrypt
+apt-get -y install ca-certificates curl postfix letsencrypt
 
 ufw allow OpenSSH
 ufw allow http
@@ -13,8 +13,7 @@ ufw enable
 curl -LO https://packages.gitlab.com/install/repositories/gitlab/gitlab-ce/script.deb.sh
 bash script.deb.sh
 
-apt-get update
-apt-get install gitlab-ce
+apt-get -y install gitlab-ce
 rm script.deb.sh
 
 gitlab-ctl reconfigure
