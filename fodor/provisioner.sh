7791c9c1e5962321aeecbdfb2597f37bb9302f07
Ashley Hindle <ashleyhindle@users.noreply.github.com>
2017-04-15 16:35:54 +0100

    Update provisioner.sh

--- fodor/provisioner.sh
+++ fodor/provisioner.sh
@@ -50,3 +50,20 @@ echo "
 " > /etc/cron.d/letsencrypt
 
 chmod 644 /etc/cron.d/letsencrypt
+
+echo "APT::Periodic::Update-Package-Lists \"1\";
+APT::Periodic::Download-Upgradeable-Packages \"1\";
+APT::Periodic::AutocleanInterval \"1\";
+APT::Periodic::Unattended-Upgrade \"1\";" > /etc/apt/apt.conf.d/10periodic
+
+echo "Unattended-Upgrade::Allowed-Origins {
+    \"${distro_id}:${distro_codename}-security\";
+    \"*packages.gitlab.com/gitlab/gitlab-ce:${distro_codename}\";
+};
+
+Unattended-Upgrade::Package-Blacklist {
+    //
+};
+
+Unattended-Upgrade::Mail \"root@localhost\";
+" >  /etc/apt/apt.conf.d/50unattended-upgrades
